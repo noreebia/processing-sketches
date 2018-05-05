39239433fbcb854b1270e6a0aac52d5e6d45edee
sooyoung-kim <noreebia@gmail.com>
2018-05-05 17:34:08 +0900

    Add further fleshed out electricity field module

---
+++ neon_racer/Individual modules/Electricity Field 2/Main/ElectricityField.js
@@ -0,0 +1,83 @@
+function ElectricityField(x1, y1, x2, y2) {
+  this.x1 = x1;
+  this.y1 = y1;
+  this.x2 = x2;
+  this.y2 = y2;
+
+  this.widthBetweenElements=10;
+  this.elements = [];
+  this.shouldFirstElementBeHigh = true;
+  this.framesToRemainStill = 3;
+
+  this.framesLeftUntilChange;
+  this.resetFramesLeftUntilChange();
+}
+
+ElectricityField.prototype.run = function(){
+    this.countDownFramesLeftUntilChange();
+    if(this.shouldElementsChange){
+        this.changeElements();
+        this.resetFramesLeftUntilChange();
+    }
+}
+
+ElectricityField.prototype.countDownFramesLeftUntilChange = function(){
+    this.framesLeftUntilChange--;
+}
+
+ElectricityField.prototype.resetFramesLeftUntilChange = function(){
+    this.framesLeftUntilChange = this.framesToRemainStill
+}
+
+ElectricityField.prototype.shouldElementsChange = function(){
+    if(this.framesLeftUntilChange == 0){
+        return true;
+    }
+    return false;
+}
+
+ElectricityField.prototype.display = function () {
+    push();
+    translate(this.x1, this.y1);
+    rotate(atan2((this.y2 - this.y1), (this.x2 - this.x1)));
+    for(i=0; i < this.elements.length; i++){
+        if(i > 0){
+            line(this.elements[i - 1].x, this.elements[i - 1].y, this.elements[i].x, this.elements[i].y);
+        }
+    }
+    pop();
+}
+
+ElectricityField.prototype.changeElements = function() {
+    this.elements = [];
+
+    this.invertField();
+    distanceOfLine = Math.sqrt(Math.pow((this.x2 - this.x1), 2) + Math.pow((this.y2 - this.y1), 2));
+    this.elements.push(new Point(0,0));
+
+    let oddEvenFluctuator;
+    if(this.shouldFirstElementBeHigh){
+        oddEvenFluctuator = 0;
+    } else{
+        oddEvenFluctuator = 1;
+    }
+    for(i=this.widthBetweenElements;i < distanceOfLine; i+=this.widthBetweenElements){
+        let elementHeight;
+        if(oddEvenFluctuator){
+            elementHeight = random(2, 12);
+        } else{
+            elementHeight = random(-2, -12);
+        } 
+        let point = new Point(i, elementHeight);
+        this.elements.push(point);
+        oddEvenFluctuator = (++oddEvenFluctuator) %2;
+        console.log("done");
+    }
+    this.elements.push(new Point(distanceOfLine,0));
+    console.log(this.elements);
+}
+
+
+ElectricityField.prototype.invertField = function(){
+    this.shouldFirstElementBeHigh = !this.shouldFirstElementBeHigh;
+}
---
+++ neon_racer/Individual modules/Electricity Field 2/Main/Main.js
@@ -0,0 +1,17 @@
+let testElectricityField;
+x=0
+
+
+function setup() {
+  createCanvas(1000, 1000);
+  testElectricityField = new ElectricityField(0, 0, 100, 100);
+}
+
+function draw() {
+  background(255);
+  line(0, 0, 100, 100);
+  testElectricityField.changeElements();
+  testElectricityField.display();
+
+  console.log(x++);
+}
